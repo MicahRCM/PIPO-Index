a1814f0177b8e93e1b7ca926e7d7b81ae74e9a1a
MicahRCM <mcorningmyers@gmail.com>
2021-01-23 16:06:03 -0500

    toolbar live test

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 const showDrop = (id) => {
-	let ids = [`dataDrop`, `toolDrop`, `researchDrop`, `aboutDrop`]
-	ids.forEach((n) => {
-		document.getElementById(n).className = "dropContent"
-		document.getElementById(n + "_1").className = "navbarItemContainer"
-	})
-	document.getElementById(id).className = "dropContent showDrop"
-	document.getElementById(id + "_1").className = "navbarItemContainer navSelected"
+    let ids = [`dataDrop`, `toolDrop`, `researchDrop`, `aboutDrop`]
+    ids.forEach((n) => {
+        document.getElementById(n).className = "dropContent"
+        document.getElementById(n + "_1").className = "navbarItemContainer"
+    })
+    let element = document.getElementById(id)
+    if (!element.selected) {
+        element.className = "dropContent showDrop"
+        document.getElementById(id + "_1").className = "navbarItemContainer navSelected"
+        document.getElementById(`headerDrop`).className = "headerDrop showDrop"
+    } else {
+    	document.getElementById(id + "_1").className = "navbarItemContainer"	
+    	document.getElementById(`headerDrop`).className = "headerDrop"
+    }
+    element.selected = !element.selected
 }
