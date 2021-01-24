be90c8bab171ff02426e6cd18e5815b41f5682ea
MicahRCM <mcorningmyers@gmail.com>
2021-01-24 15:18:15 -0500

    minor minification, repathing

--- js/index.js
+++ js/index.js
@@ -1,26 +1,3 @@
-let lastid = ""
-
-const showDrop = (id) => {
-    let ids = [`dataDrop`, `toolDrop`, `researchDrop`, `aboutDrop`]
-    ids.forEach((n) => {
-        document.getElementById(n).className = "dropContent"
-        document.getElementById(n + "_1").className = "navbarItemContainer"
-    })
-    let element = document.getElementById(id)
-    if (!element.selected) {
-        element.className = "dropContent showDrop"
-        document.getElementById(id + "_1").className = "navbarItemContainer navSelected"
-        document.getElementById(`headerDrop`).className = "headerDrop showDrop"
-    } else {
-    	document.getElementById(id + "_1").className = "navbarItemContainer"	
-    	document.getElementById(`headerDrop`).className = "headerDrop"
-    }
-    lastid = id
-    element.selected = !element.selected
-}
-
-window.addEventListener('mousedown', e => {
-    if (event.target.matches('.makeOpaque')) {
-    	showDrop(lastid)
-    }
+$(function(){
+  $("#nav-placeholder").load("toolbar.html");
 });
