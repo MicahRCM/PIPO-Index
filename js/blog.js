f2a706ae923337a0941e3e7724e34785601bc7a0
Micah Corning-Myers <mcorningmyers@gmail.com>
2021-04-10 15:56:40 -0500

    new quadrants added, no links

---
+++ js/blog.js
@@ -0,0 +1,10 @@
+$(function(){
+  $("#nav-placeholder").load("toolbar.html");
+});
+
+const genEntries = () => {
+	for (let i = 0; i < ENTRIES.length; i++) {
+		let container = document.createElement(`div`)
+		container.classList.add("entry_container")
+	}
+}
