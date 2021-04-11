565a1e0cacadfe6d8603d210c5e417b843526aed
Micah Corning-Myers <mcorningmyers@gmail.com>
2021-04-10 19:49:55 -0500

    blog page update

--- js/blog.js
+++ js/blog.js
@@ -1,10 +1,11 @@
 $(function(){
-  $("#nav-placeholder").load("toolbar.html");
+  $("#nav-placeholder").load("../toolbar.html");
 });
 
 const genEntries = () => {
 	for (let i = 0; i < ENTRIES.length; i++) {
 		let container = document.createElement(`div`)
 		container.classList.add("entry_container")
+
 	}
 }
